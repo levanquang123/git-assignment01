b262df507ad98d50601fb2199cecc409c2f93096
levanquang27122005-tech <levanquang27122005@gmail.com>
2026-06-21 08:57:54 +0700

    feat: add hello button

--- GitAssignmentApp/app/src/main/java/com/example/gitassignmentapp/MainActivity.kt
+++ GitAssignmentApp/app/src/main/java/com/example/gitassignmentapp/MainActivity.kt
@@ -1,47 +1,44 @@
 package com.example.gitassignmentapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
 import com.example.gitassignmentapp.ui.theme.GitAssignmentAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             GitAssignmentAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HelloButtonScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun HelloButtonScreen() {
+    val context = LocalContext.current
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GitAssignmentAppTheme {
-        Greeting("Android")
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Button(
+            onClick = {
+                Toast.makeText(context, "xin chào", Toast.LENGTH_SHORT).show()
+            }
+        ) {
+            Text(text = "Xin chào")
+        }
     }
 }
