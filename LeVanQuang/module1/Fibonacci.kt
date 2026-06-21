73f46fd1df6b008cd74571951e3762b6326858ee
levanquang27122005-tech <levanquang27122005@gmail.com>
2026-06-21 14:11:49 +0700

    feat: implement iterative fibonacci

--- LeVanQuang/module1/Fibonacci.kt
+++ LeVanQuang/module1/Fibonacci.kt
@@ -1,3 +1,26 @@
 interface Fibonacci {
     fun fibonacci(n: Int, flag: Boolean): Int
 }
+
+class FibonacciImpl2 : Fibonacci {
+    override fun fibonacci(n: Int, flag: Boolean): Int {
+        if (flag) {
+            return -1
+        }
+
+        if (n <= 1) {
+            return n
+        }
+
+        var previous = 0
+        var current = 1
+
+        for (i in 2..n) {
+            val next = previous + current
+            previous = current
+            current = next
+        }
+
+        return current
+    }
+}
